4d466b965696ccb814d2dc9c174d49adc402ba00
Ashay Maheshwari <aaryaashay1848@gmail.com>
2016-10-03 09:54:21 +0530

    Update common.js

--- src/common.js
+++ src/common.js
@@ -13,7 +13,7 @@
  var time_now = hours + ":" + minutes
  var long_usrid = "%%USER_ID%%"; //get the User ID as a string  
 
- var server_url = "http://vlabs-analytics.vlabs.ac.in:4000/"
+ var server_url = "http://analytics-api.vlabs.ac.in:4000/"
 
  var xhttp = new XMLHttpRequest();
  var urlstr = server_url + long_usrid + "," + courseid + "," + date_today + "," + time_now + "," + experiment_name + "," + lab_name
