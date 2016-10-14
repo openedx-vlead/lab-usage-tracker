0ca22cb29e451aeb91c7dee783522f776149e602
Ashay Maheshwari <aaryaashay1848@gmail.com>
2016-10-14 15:49:20 +0530

    Update labEventtracker.py

--- src/vlabs-tracking/labEventTracker.py
+++ src/vlabs-tracking/labEventTracker.py
@@ -1,11 +1,17 @@
 import os
 import json
+import time
+import datetime
 from flask import Flask
 from flask import send_from_directory
 from flask import request
 from elasticsearch import Elasticsearch
+from flask_cors import CORS, cross_origin
+import requests
+import ast
 
 app = Flask(__name__)
+CORS(app)
 # app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/favicon.ico')
@@ -13,25 +19,39 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@app.route('/')
+def hello():
+    return "Hello World!"
 
-@app.route('/<analytics>',methods= ['GET'])
+@app.route('/<analytics>',methods= ['GET','POST'])
 def fetch_analytics(analytics):
     	data = request.data
-    	fetch_data = name
+    	fetch_data = analytics
     	data_list = fetch_data.split(",") 
     	data_dict = {}
-
-    	data_dict["STUDENT_LONG_ID"] = data_list[0]
-    	data_dict["COURSE_ID"] = data_list[1]
-    	data_dict["DATE_OF_EXPERIMENT"] = data_list[2]	   
-    	data_dict["TIME_OF_EXPERIMENT"] = data_list[3]
+        today = str(datetime.date.today())
+    	data_dict["STUDENT_ID"] = data_list[0]
+    	data_dict["LAB_ID"] = data_list[1]
+	data_dict["LAB_NAME"] = data_list[2]
+	#data_dict["EXPERIMENT_NAME"] = data_list[3] + "-" + data_list[2]
+	#data_dict["EXPERIMENT_ID"] = data_list[4]
+	data_dict["EXPERIMENT_ID"] = data_list[3]
 	data_dict["EXPERIMENT_NAME"] = data_list[4]
-	data_dict["LAB_NAME"] = data_list[5]
+	data_dict["DATE_OF_EXPERIMENT"] = today
+	data_dict["TIME_OF_EXPERIMENT"] = time.strftime("%H:%M")
 	data_dict["IP_ADDRESS"] = request.environ.get('HTTP_X_REAL_IP',request.remote_addr)
 
+	res = requests.get("http://freegeoip.net/json/" + data_dict["IP_ADDRESS"])
+	data = res.text
+	ip_dir = ast.literal_eval(data)
+	data_dict["REGION"] = ip_dir["region_name"]
+	#print type(ip_dir)
+	#print ip_dir
+
+    	json_data = json.dumps(data_dict)
 	try:
-		es = Elasticsearch([{'host':'elk-stack.vlabs.ac.in', 'port':9200}])
-		es.index(index="vlabs", doc_type="usage", body=data_dict)
+		es = Elasticsearch([{'host':'some-server.domain', 'port':some-port}])
+		es.index(index="some-indexx", doc_type="some-doc-type", body=data_dict)
 	except Exception as e:
 		print e
 	
@@ -39,6 +59,4 @@ def fetch_analytics(analytics):
 	return "Hello {}!".format(analytics) 
 
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(host = "0.0.0.0")
