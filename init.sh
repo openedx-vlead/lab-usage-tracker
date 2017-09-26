5b73bba7dfa884a8699404db48c3bc3ee4ed8657
ksripathi <kammari.sripathi@gmail.com>
2017-09-26 16:02:15 +0530

    Added init.sh file

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -d literate-tools ]; then
+    echo "literate-tools already present"
+else
+    git clone https://github.com/vlead/literate-tools.git
+fi
+
+if [ -L tangle-make ]; then
+    echo "symlinked makefile already present"
+else 
+    ln -sf literate-tools/makefile tangle-make
+fi
+
